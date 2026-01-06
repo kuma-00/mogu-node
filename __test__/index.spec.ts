26e23acdc531201ab23418028f691d7d023a29c4
kuma-00 <kuma.gact@gmail.com>
2026-01-06 17:33:00 +0000

    test: replace template tests with Mogu-specific tests - Remove placeholder plus100 test - Add tests for MoguImageProcessor, MoguFoodDetector, and MoguTensor exports - Verify factory method withImagenetNormalization works

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -1,8 +1,28 @@
 import test from 'ava'
 
-import { plus100 } from '../index'
+import { MoguImageProcessor, MoguFoodDetector, MoguTensor } from '../index'
 
-test('sync function from native code', (t) => {
-  const fixture = 42
-  t.is(plus100(fixture), fixture + 100)
+test('MoguImageProcessor is exported', (t) => {
+  t.truthy(MoguImageProcessor)
+  t.is(typeof MoguImageProcessor, 'function')
+})
+
+test('MoguFoodDetector is exported', (t) => {
+  t.truthy(MoguFoodDetector)
+  t.is(typeof MoguFoodDetector, 'function')
+})
+
+test('MoguTensor is exported', (t) => {
+  t.truthy(MoguTensor)
+  t.is(typeof MoguTensor, 'function')
+})
+
+test('MoguImageProcessor can be instantiated', (t) => {
+  const processor = new MoguImageProcessor(224, 224)
+  t.truthy(processor)
+})
+
+test('MoguImageProcessor.withImagenetNormalization factory method works', (t) => {
+  const processor = MoguImageProcessor.withImagenetNormalization(224, 224)
+  t.truthy(processor)
 })
